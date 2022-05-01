04ed57f41af1ab1eda56087a6ee59e76e2b86c39
dahee333 <21700765@handong.ac.kr>
2022-05-01 19:32:27 +0900

    App.java, Member.java 변경 checkin 추가

--- src/main/java/com/oodp/project/App.java
+++ src/main/java/com/oodp/project/App.java
@@ -4,18 +4,24 @@ import com.oodp.project.repository.MemberRepository;
 import com.oodp.project.repository.MemoryMemberRepository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
 import java.util.HashMap;
 import java.util.Map;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner strSc = new Scanner(System.in);
         char c;
         String str;
         System.out.println("WELCOME TO PET HOTEL!");
         Staff s = new Staff("1","admin","admin",50000);
         MemberRepository memberRepository = new MemoryMemberRepository(); // repository
+
 
         while(true) {
             System.out.println("---------------------------------------");
@@ -43,7 +49,37 @@ public class App {
                 }
             }
             else if(c == '2') {
-                System.out.println("------------------------------");
+//                System.out.println("------------------------------");
+//
+//                System.out.println("IN MEMBER MODE");
+//                System.out.println("------------------------------");
+//                Member member = new Member("1", new ArrayList<>(),0);
+//                CheckIn_Info check = new CheckIn_Info(member, new ArrayList<>(), new ArrayList<>(), 0, new Date(), new Date());
+//
+//                //System.out.println("CHOOSE MEMBER");
+//                //memberRepository.showAll();
+//
+//                while(true){
+//
+//                    System.out.println("CHOOSE MENU: CHECK-IN(1) RETURN(2)");
+//                    System.out.println("------------------------------");
+//
+//                    c = sc.next().charAt(0);
+//                    if(c =='1') {
+//                        System.out.println("------------------------------");
+//                        System.out.println("Need some information for check-in ");
+//                        System.out.println("------------------------------");
+//
+//                        typeMonitor(check);
+//                        typeInfo(check);
+//                        printInfo(check);
+//
+//                    }
+//                    else if(c =='2') {
+//                        break;
+//                    }
+//                 }
+
                 System.out.println("[MEMBER MODE]");
 
 
@@ -65,10 +101,10 @@ public class App {
 //                        System.out.println("getMemberID(): " + m.getMemberID());
                         while(true) {
                             System.out.println("---------------------------");
-                            System.out.println("CHOOSE MENU:\n1. SHOW PET LIST\n2. ADD PET INFO\n3. EDIT PET INFO\n4. DELETE PET INFO\n5. RETURN");
+                            System.out.println("CHOOSE MENU:\n1. SHOW PET LIST\n2. ADD PET INFO\n3. EDIT PET INFO\n4. DELETE PET INFO\n5. CHECK_IN\n6. RETURN");
                             c = sc.next().charAt(0);
                             if (c == '1') {
-                               m.viewPetList();
+                                m.viewPetList();
                             }
                             else if (c =='2') {
                                 System.out.println("Please Enter Your Pet name.");
@@ -81,20 +117,81 @@ public class App {
                             else if (c =='4') {
                                 System.out.println("not implemented yet...");
                             }
-                            else if (c == '5') {
+                            else if(c =='5') {
+                                if(m.getPetList().size() == 0){
+                                    m.viewPetList();
+                                    continue;
+                                }
+                                m.viewPetList();
+
+                                System.out.println("------------------------------");
+                                System.out.println("Type a petID for Check-in");
+                                System.out.println("------------------------------");
+
+                                String petID = sc.next();
+                                if(m.findByID(petID).getPetID() == "0") continue;
+
+                                CheckIn_Info check = new CheckIn_Info(m, m.findByID(petID), new ArrayList<>(), new ArrayList<>(), 0, new Date(), new Date());
+
+                                typeMonitor(check);
+                                typeInfo(check);
+                                printInfo(check);
+                            }
+                            else if(c == '6'){
                                 break;
                             }
-
                         }
-
-
                     }
                 }
-
             }
             else break;
-
         }
+    }
+    public static void typeMonitor(CheckIn_Info check){
+        Scanner sc = new Scanner(System.in);
+        char c;
+        do{
+            System.out.println("MonitorType : Sleep(1) Walk(2) Eat(3) Groom(4) Shower(5) EarClean(6) NailCut(7) RETURN(0)");
+            c = sc.next().charAt(0);
 
+            switch (c){
+                case '1': check.addMonitortype(MonitorType.Sleep); break;
+                case '2': check.addMonitortype(MonitorType.Walk); break;
+                case '3': check.addMonitortype(MonitorType.Eat); break;
+                case '4': check.addMonitortype(MonitorType.Groom); break;
+                case '5': check.addMonitortype(MonitorType.Shower); break;
+                case '6': check.addMonitortype(MonitorType.EarClean); break;
+                case '7': check.addMonitortype(MonitorType.NailCut); break;
+                case '0': break;
+
+            }
+        }while(c != '0');
+    }
+    public static void typeInfo(CheckIn_Info check){
+        Scanner strSc = new Scanner(System.in);
+        String str;
+        System.out.println("Type Start Day (ex: Sat, 02 Apr 2022)");
+        str = strSc.nextLine();
+        check.setStartDate(new Date(str));
+        System.out.println("Type End Day (ex: Sun, 03 Apr 2022)");
+        str = strSc.nextLine();
+        check.setEndDate(new Date(str));
+
+        System.out.println("Requirement : ");
+        str = strSc.nextLine();
+
+        check.setRequirement(Arrays.asList(str));
+    }
+
+    public static void printInfo(CheckIn_Info check){
+        System.out.println("------------------------------");
+
+        System.out.println("PetID : " + check.getPet().getPetID());
+        System.out.println("PetName : " + check.getPet().getPetName());
+        System.out.println("MonitorType : " + check.getMonitorTypes());
+        System.out.println("Start Day : " + check.getStartDate());
+        System.out.println("End Day : " + check.getEndDate());
+        System.out.println("Your requirement : " + check.getRequirement());
+        System.out.println("Total price : " + check.getPrice());
     }
 }
